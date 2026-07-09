038c608d14a9b8106350ef1df1ad4fcfb913de01
Савелий Вотинцев <votintcev1@mail.ru>
2026-07-09 17:31:20 +0300

    Понижен лимит золотого рудника до 1

---
+++ Domiki/Data/Migrations/20260709100000_Stage6GoldMineCap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domiki.Web.Data.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20260709100000_Stage6GoldMineCap")]
+    public partial class Stage6GoldMineCap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"DomikTypes\" SET \"MaxCount\" = 1 WHERE \"LogicName\" = 'gold_mine';");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"DomikTypes\" SET \"MaxCount\" = 2 WHERE \"LogicName\" = 'gold_mine';");
+        }
+    }
+}
