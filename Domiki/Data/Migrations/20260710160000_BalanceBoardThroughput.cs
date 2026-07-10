6179cc7d6701f8f9081d74ad779c7c5021c982de
Савелий Вотинцев <votintcev1@mail.ru>
2026-07-10 18:48:41 +0300

    Ускоренная выработка доски

---
+++ Domiki/Data/Migrations/20260710160000_BalanceBoardThroughput.cs
@@ -0,0 +1,23 @@
+﻿using Domiki.Web.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domiki.Web.Data.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20260710160000_BalanceBoardThroughput")]
+    public partial class BalanceBoardThroughput : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"Receipts\" SET \"DurationSeconds\" = 900 WHERE \"Id\" = 23;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"Receipts\" SET \"DurationSeconds\" = 1800 WHERE \"Id\" = 23;");
+        }
+    }
+}
